b96ec24ba8f502927ab79a2016624da01981ebd9
Kara Krzystan <kara.krzystan@gmail.com>
2021-04-11 17:59:24 -0700

    Start controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,17 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+
+//GET all thoughts
+//GET thought by id
+//POST thought
+//PUT update thought
+//DELETE thought
+
+//**Reactions are in thoughts**
+//POST reaction
+//DELETE reaction
+
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,16 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+
+//GET all users
+//GET user by id
+//POST user
+//PUT update user
+//DELETE user
+
+//POST friend
+//DELETE friend
+
+};
+
+module.exports = userController;
